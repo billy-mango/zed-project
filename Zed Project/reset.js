28dcae6b1d5f9856e518f39683002a3e9cfe114b
Billy Mango <thebillymango@gmail.com>
2025-03-21 11:05:52 +0300

    Added a div for displaying input when the radio button is clicked linked the reset html and reset js files created the append function

---
+++ Zed Project/reset.js
@@ -0,0 +1,5 @@
+// Create a new element to display the input box
+function displayInput() {
+    const input = document.createElement('input');
+    document.getElementById('input').appendChild(input);
+}
